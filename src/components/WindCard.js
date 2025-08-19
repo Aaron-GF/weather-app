d155b3d7f5c33e069fa3be7bceff30075c4cb90d
Aaron-GF <aarongarcia567@gmail.com>
2025-08-19 17:12:23 +0200

    upgrade styles from weather info

--- src/components/WindCard.js
+++ src/components/WindCard.js
@@ -4,30 +4,32 @@ import { FiWind } from "react-icons/fi";
 
 export default function WindCard({ weather }) {
   return (
-    <div className="glass-effect col-span-2 row-span-1 p-3">
+    <div className="glass-effect col-span-2 row-span-1 p-3 flex flex-col">
       <h3 className="flex gap-1.5 items-center text-sky-300 text-sm">
         <FiWind className="text-xl" />
         WIND
       </h3>
-      <div className="flex gap-80">
-        <div className="flex flex-col gap-2">
+      <div className="flex justify-around text-sm sm:text-base">
+        <div className="flex flex-col gap-2 justify-center ">
           <span>Speed {weather.wind.speed} Km/h</span>
           <span>
             Direction {weather.wind.deg}º {degToDir8(weather.wind.deg)}
           </span>
         </div>
-        <span
-          className="inline"
-          style={{
-            transform: `rotate(${weather.wind.deg}deg)`,
-          }}
-        >
-          <img
-            src="/images/icons/arrow.png"
-            alt="blue arrow"
-            className="w-20"
-          />
-        </span>
+        <div className="relative w-60 h-30 text-orange-300">
+            <span className="absolute top-0 right-4/10">North</span>
+            <span className="absolute bottom-0 right-4/10">South</span>
+            <span className="absolute left-0 top-5/10">West</span>
+            <span className="absolute right-0 top-5/10">East</span>
+            <span
+              className="flex absolute top-2/10 right-4/10"
+              style={{
+                transform: `rotate(${weather.wind.deg}deg)`,
+              }}
+            >
+                <img src="/images/icons/arrow.png" alt="blue arrow" />
+            </span>
+        </div>
       </div>
     </div>
   );
